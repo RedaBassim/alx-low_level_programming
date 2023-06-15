fbb2b03e37e68d6379f5aa9a4ae2ebb7bccdf43b
RedaBassim <yassinharti34@gmail.com>
2023-06-15 18:39:14 +0100

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - main entery points
-*return: is the value that is returned from the function to the caller
-**/
+ * main - Entry point of the program
+ *
+ * Description: Program that prints what is demanded
+ *
+ * Return: Always 0 (indicating success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
