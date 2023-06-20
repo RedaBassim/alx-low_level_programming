2978c1a18bab82447ac757954a90d775d850705c
RedaBassim <yassinharti34@gmail.com>
2023-06-20 13:35:01 +0100

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * print_alphabet - utilizes on th _putchar
+ *
+ */
+
+void print_alphabet(void);
+
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
